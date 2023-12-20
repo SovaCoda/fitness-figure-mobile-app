1308f47e82b5a9891443cf9716b9c29108ef83fc
Alex Jiang <alexjiang04@gmail.com>
2023-12-20 18:20:40 -0500

    fixed directory

--- ffapp/test/widget_test.dart
+++ ffapp/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ffapp/client/main.dart';
+import 'package:ffapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
