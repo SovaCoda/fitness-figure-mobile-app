758a540e4b6d9a22527d82bd43584f7e227ab92d
Conner Bell <chb263@msstate.edu>
2024-03-26 02:42:46 -0500

    added figure instance backend support

--- ffapp/lib/pages/home/inventory.dart
+++ ffapp/lib/pages/home/inventory.dart
@@ -1,14 +1,11 @@
 import 'package:ffapp/components/inventory_item.dart';
 import 'package:ffapp/main.dart';
 import 'package:flutter/material.dart';
-import 'package:ffapp/services/flutterUser.dart';
 import 'package:go_router/go_router.dart';
-import 'package:logger/logger.dart';
 import 'package:ffapp/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'package:ffapp/services/routes.pb.dart' as Routes;
-import 'package:fixnum/fixnum.dart';
-import 'package:ffapp/pages/home/store.dart';
+
 
 class Inventory extends StatefulWidget {
   const Inventory({Key? key}) : super(key: key);
@@ -27,6 +24,8 @@ class _InventoryState extends State<Inventory> {
   late AuthService auth;
   late int currency = 0;
 
+  late List<Routes.FigureInstance>  figureInstancesList = List.empty();
+
   void initState() {
     super.initState();
     auth = Provider.of<AuthService>(context, listen: false);
@@ -37,6 +36,11 @@ class _InventoryState extends State<Inventory> {
     Routes.User? databaseUser = await auth.getUserDBInfo();
     String stringCur = databaseUser?.currency.toString() ?? "0";
     currency = int.parse(stringCur);
+    await auth.getFigureInstances(databaseUser!).then((value) => setState(() {
+      figureInstancesList = value.figureInstances;
+    }));
+
+
   }
 
   @override
@@ -53,18 +57,22 @@ class _InventoryState extends State<Inventory> {
             // generates the store as a bunch of rows with 2 elements each from the array above
             // TO DO: if there are an odd number of skins it wont render the last one rn
             children: List.generate(
-                (listOfSkins.length / 2).floor(),
+                (figureInstancesList.length / 2).floor(),
                 (index) => Column(children: [
                       const SizedBox(height: 15),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           const SizedBox(width: 5),
-                          InventoryItem(
-                              photoPath: listOfSkins[index * 2][0].toString(),
-                              onViewDetails: (context) => {context.goNamed("FigureDetails", pathParameters: {'figureUrl': listOfSkins[index * 2][0].toString()})},
-                              equiped: listOfSkins[index * 2][1].toString() == "true" ? true : false,
-                              onEquip: (context) => {}
+                          Consumer<UserModel>(
+                            builder: (context, userModel, _) {
+                              return InventoryItem(
+                                photoPath: figureInstancesList[index * 2].figureName.toString(),
+                                onViewDetails: (context) => {context.goNamed("FigureDetails", pathParameters: {'figureUrl': figureInstancesList[index * 2].figureName.toString()})},
+                                equiped: figureInstancesList[index * 2].figureName.toString() == userModel.user?.curFigure ? true : false,
+                                onEquip: (context) => {}
+                              );
+                            },
                           ),
                           const SizedBox(width: 15),
                           InventoryItem(
