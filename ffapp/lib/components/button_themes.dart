dcad329a622be15efd84d2be67ddd79a5f3cb02d
ed843 <ed843@msstate.edu>
2024-08-07 21:20:26 -0500

    Improved research menu
    Things that still need to be added:
    - A provider of the user's available research
    - Random EV and duration values on research
    - $/sec value updated depending on EV level

--- ffapp/lib/components/button_themes.dart
+++ ffapp/lib/components/button_themes.dart
@@ -157,4 +157,85 @@ class FfButtonProgressable extends StatelessWidget {
       ),
     );
   }
+  
 }
+
+class FfButtonProgressableResearch extends StatelessWidget {
+  final String text;
+  final IconData? icon;
+  final Color textColor;
+  final Color backgroundColor;
+  final Color disabledColor;
+  final VoidCallback onPressed;
+  final double height;
+  final double width;
+  final double iconSize;
+  final double progress;
+  final bool disabled;
+  final TextStyle textStyle;
+  
+
+  const FfButtonProgressableResearch({
+    super.key,
+    required this.text,
+    this.icon,
+    required this.textStyle,
+    required this.textColor,
+    required this.backgroundColor,
+    required this.onPressed,
+    required this.progress,
+    required this.disabledColor,
+    this.disabled = false,
+    required this.width,
+    required this.height,
+    this.iconSize = 20,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(13),
+        gradient: LinearGradient(colors: [backgroundColor, backgroundColor, disabledColor, disabledColor], stops: [0, progress -0.05, progress, 1])
+      ),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+            const EdgeInsets.symmetric(horizontal: 0), // remove default padding
+          ),
+          shape: WidgetStateProperty.all<OutlinedBorder>(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(13)),
+          ),
+          
+          backgroundColor: WidgetStateProperty.all<Color>(
+              Colors.transparent),
+        ),
+        onPressed: disabled ? null : progress == 1 ? onPressed : null,
+        child: icon != null
+            ? Stack(
+                children: [
+                  Positioned(
+                    left: 20,
+                    top: height / 2 - iconSize / 2,
+                    child: Icon(icon, color: textColor, size: iconSize),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(text,
+                            style: textStyle.copyWith(color: textColor)),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            : Text(text, style: textStyle.copyWith(color: textColor)),
+      ),
+    );
+  }
+}
+
+
---
+++ ffapp/lib/components/robot_line_painter.dart
@@ -0,0 +1,53 @@
+import "package:flutter/material.dart";
+
+class RobotLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = const Color(0xFF59FF00)
+      ..strokeWidth = 5
+      ..style = PaintingStyle.stroke;
+
+    Offset startPoint = Offset(size.width * 0.6, size.height * 0.45);
+    Offset endPoint = Offset(size.width * 0.90, size.height * 0.45);
+
+
+    // Draw three lines
+    for (int i = 0; i < 3; i++) {
+      double yOffset = (i - 1) * 40;
+      canvas.drawLine(
+        startPoint.translate(0, yOffset),
+        endPoint.translate(0, yOffset),
+        paint,
+      );
+    }
+    startPoint = Offset(size.width * 0.896, size.height *0.45);
+    endPoint = Offset(size.width * 1.02, size.height * 0.40);
+
+    canvas.drawLine(
+      startPoint.translate(0, 40),
+      endPoint.translate(0, 40),
+      paint,
+  );
+  startPoint = Offset(size.width * 0.896, size.height *0.45);
+    endPoint = Offset(size.width * 1.02, size.height * 0.45);
+    canvas.drawLine(
+      startPoint.translate(0, 0),
+      endPoint.translate(0, 0),
+      paint,
+  );
+
+  startPoint = Offset(size.width * 0.899, size.height * 0.45);
+    endPoint = Offset(size.width * 1.055, size.height * 0.50);
+    canvas.drawLine(
+      startPoint.translate(0, -40),
+      endPoint.translate(0, -40),
+      paint,
+  );
+  }
+
+  
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
